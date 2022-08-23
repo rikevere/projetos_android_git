842e60f10a7231e7405ba616d65364f4fb2935f4
Ricardo Ludwig <rikevere@gmail.com>
2022-08-23 17:17:15 -0300

    Início parte 2.2 Curso - Refatorando Código - parte 2

--- Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -2,12 +2,11 @@ package br.com.alura.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,7 +18,7 @@ import br.com.alura.agenda.model.Aluno;
 public class ListaAlunosActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Lista de alunos";
-    private ListView listaDeAlunos;
+    private ListView listViewDelistaDeAlunos;
     private FloatingActionButton botaoNovoAluno;
     private final AlunoDAO dao = new AlunoDAO();
 
@@ -40,7 +39,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                abreFormularioAlunoActivity();
+                abreFormularioModoEditaAluno();
             }
         });
     }
@@ -51,10 +50,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
         // o comando findViewById busca os componentes
         // dentro do layout inicializado no comando "setContentView"
         botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
-        listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        listViewDelistaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
     }
 
-    private void abreFormularioAlunoActivity() {
+    private void abreFormularioModoEditaAluno() {
         //        Exemplo de opção para iniciar uma nova intenção "Intent" de abertura
         //        de um formulário (layout), (Activity)
         //        Intent i = new Intent(this, FormularioAlunoActivity.class);
@@ -73,27 +72,39 @@ public class ListaAlunosActivity extends AppCompatActivity {
     private void configuraLista() {
         //o comando abaixo adapta uma lista ao componente "listview" fixado no layout
         List<Aluno> alunos = dao.todos();
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+        configuraAdapter(alunos);
+        coonfiguraListenerDeCliquePorItem(alunos);
+
+    }
+
+    private void coonfiguraListenerDeCliquePorItem(List<Aluno> alunos) {
+        listViewDelistaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Aluno alunoEscolhido = (Aluno) listViewDelistaDeAlunos.getItemAtPosition(position);
+                abreFormularioModoEditaAluno(alunoEscolhido);
+            }
+        });
+    }
+
+    private void abreFormularioModoEditaAluno(Aluno alunoEscolhido) {
+        //comando abaixo cria uma nova variável "Intent" que aponta para qual activity eu quero abrir
+        Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
+        //Esta variável conta com um recurso chamado "putExtra", que permite enviar dados para outra activity
+        //mas para que funcione o recurso precisa ser serializável (transformado para Bite)
+        //de modo que a classe de origem precise ser declarada como "implements Serializable"
+        //neste caso a classe "aluno"
+        vaiParaFormularioActivity.putExtra("aluno", alunoEscolhido);
+        startActivity(vaiParaFormularioActivity);
+    }
+
+    private void configuraAdapter(List<Aluno> alunos) {
+        listViewDelistaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 alunos));
-        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Aluno alunoEscolhido = alunos.get(position);
-                Toast.makeText(ListaAlunosActivity.this, "" + alunoEscolhido, Toast.LENGTH_SHORT).show();
-                Log.i("posicao aluno", "" + alunoEscolhido);
-                //comando abaixo cria uma nova variável "Intent" que aponta para qual activity eu quero abrir
-                Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
-                //Esta variável conta com um recurso chamado "putExtra", que permite enviar dados para outra activity
-                //mas para que funcione o recurso precisa ser serializável (transformado para Bite)
-                //de modo que a classe de origem precise ser declarada como "implements Serializable"
-                //neste caso a classe "aluno"
-                vaiParaFormularioActivity.putExtra("aluno", alunoEscolhido);
-                startActivity(vaiParaFormularioActivity);
-            }
-        });
+    }
 
-        };
+    ;
  }
 
